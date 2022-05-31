8a88b38986dab5d1153bc5004da886a065243960
Olaife Olawore <olaifeolawore@gmail.com>
2022-05-31 06:32:07 -0700

    modified 5 0x01

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+delimiter = ", "
 for num in range(0, 100):
     if num < 10:
-        print("0{}".format(num), end=", ")
+        print("0{}".format(num), end=delimiter)
     else:
-        print("{}".format(num), end=", ")
+        if num == 99:
+            delimiter = "\n"
+        print("{}".format(num), end=delimiter)
