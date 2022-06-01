53e5e891bd4ed920f4bd4b8d35d732f05acbb50e
Olaife Olawore <olaifeolawore@gmail.com>
2022-06-01 05:41:27 -0700

    10, 11, 12, 13, 100, 101, 102 0x01 done

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * insert_node: inserts a number into a sorted linked_list
+ *
+ * @head: address of the first element of the list
+ * @number: integer number to add
+ *
+ * Return: address of the new node or NULL
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	
+}
