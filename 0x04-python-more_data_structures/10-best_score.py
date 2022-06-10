71826e8c9cce2f1c6852a98b7bf808e159a59b7e
Olaife Olawore <olaifeolawore@gmail.com>
2022-06-10 12:08:34 -0700

    7-11 0x04 done

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max = 0
+    if not a_dictionary:
+        return None
+    for k, v in a_dictionary.items():
+        if v > max:
+            max = v
+    return max
