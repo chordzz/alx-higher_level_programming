6b104563825ff2f8f6cfcf514b7cb786ef04fc59
Olaife Olawore <olaifeolawore@gmail.com>
2022-06-21 08:26:47 -0700

    0,1 done 0x06, readme.md created

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Class called square with attribute called size"""
+class Square:
+    """Class called Square with attribute called size"""
+    def __init__(self, size):
+        """Square Constructor"""
+        self.__size = square_size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+### Directory on python classes and OOP
