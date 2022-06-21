4d3545f793224c33f0ec2defb97df5c3a2c7ae6f
Olaife Olawore <olaifeolawore@gmail.com>
2022-06-21 08:36:59 -0700

    test

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,6 +2,6 @@
 """Class called square with attribute called size"""
 class Square:
     """Class called Square with attribute called size"""
-    def __init__(self, size):
+    def __init__(self, square_size):
         """Square Constructor"""
-        self.__size = size
+        self.__size = square_size
