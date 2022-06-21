bf3f303425ab88bc8edc415291b15b76cde70265
Olaife Olawore <olaifeolawore@gmail.com>
2022-06-21 06:01:00 -0700

    Created 0x05, 0-1 done, readme file created

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for count in range(x):
+        try:
+            print("{}".format(my_list[count]), end="")
+        except IndexError:
+            continue
+
+        counter += 1
+    print()
+    return count
