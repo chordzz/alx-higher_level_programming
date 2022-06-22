0185282d202c1d49a05341f5f7dbb63d716dfb10
Olaife Olawore <olaifeolawore@gmail.com>
2022-06-22 08:27:21 -0700

    added mod to 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,7 +19,7 @@ class Square:
         if (self.__size == 0):
             print()
         else:
-            for i in range(self.__position[1]):
+            for i in range(self.position[1]):
                 print()
             for j in range(self.__size):
                 print(" " * self.position[0], end="")
