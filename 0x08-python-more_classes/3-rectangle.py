30f8558bf7b197bd766b71d0643f149690202a98
Olaife Olawore <olaifeolawore@gmail.com>
2022-06-27 08:38:49 -0700

    redid 3-rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -40,12 +40,11 @@ class Rectangle:
             - Value: int
         """
 
+        if type(value) != int:
+            raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
-        elif type(value) != int:
-            raise TypeError('width must be an integer')
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -64,12 +63,11 @@ class Rectangle:
             - Value: int
         """
 
+        if (type(value) != int):
+            raise TypeError('height must be an integer'
         if value < 0:
             raise ValueError('height must be >= 0')
-        elif type(value) != int:
-            raise TypeError('height must be an integer')
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
         """
